311247f6263db3166ae73f58ccb1a4e3a2c40cc7
mambax7 <mambax7@gmail.com>
2017-07-27 14:30:48 -0400

    updates

--- xoops_version.php
+++ xoops_version.php
@@ -1,15 +1,14 @@
 <?php
-$modversion['name'] = "xoops-styles";
-$modversion['version'] = 1.03;
-$modversion['description'] = "Faux module Xoops Documentation";
-$modversion['credits'] = "Chewb - Christian - Dugris - Kris_fr";
-$modversion['author'] = "Burning & Xoops Design Team";
-$modversion['help'] = "";
-$modversion['license'] = "GPL see LICENSE";
-$modversion['official'] = 1;
-$modversion['image'] = "docs_slogo.png";
-$modversion['dirname'] = "xoops-styles";
+$modversion['version']     = 1.03;
+$modversion['name']        = 'xoops-styles';
+$modversion['description'] = 'Faux module Xoops Documentation';
+$modversion['credits']     = 'Chewb - Christian - Dugris - Kris_fr';
+$modversion['author']      = 'Burning & Xoops Design Team';
+$modversion['help']        = '';
+$modversion['license']     = 'GPL see LICENSE';
+$modversion['official']    = 1;
+$modversion['image']       = 'assets/images/logoModule.png';
+$modversion['dirname']     = 'xoops-styles';
 
 // Menu
 $modversion['hasMain'] = 1;
-?>
